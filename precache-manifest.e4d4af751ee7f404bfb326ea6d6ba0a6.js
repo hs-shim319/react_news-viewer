5a81d0a8ca4baa9fb94ceb9112d938a9a72ed5a8
hsshim <hsshim319@naver.com>
2020-09-22 19:13:03 +0900

    Updates

---
+++ precache-manifest.e4d4af751ee7f404bfb326ea6d6ba0a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8174b1461e950a63b828e0183cfbea",
+    "url": "/react_news-viewer/index.html"
+  },
+  {
+    "revision": "2167453769b1fb37e050",
+    "url": "/react_news-viewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc3a9298934b473123f2",
+    "url": "/react_news-viewer/static/js/2.feea3b49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_news-viewer/static/js/2.feea3b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2167453769b1fb37e050",
+    "url": "/react_news-viewer/static/js/main.469f2840.chunk.js"
+  },
+  {
+    "revision": "cd387614022aa95c0dc6",
+    "url": "/react_news-viewer/static/js/runtime-main.1b162b69.js"
+  }
+]);
